469fff43b11a6ab7e5b07d8dcae3d225c5c6ea14
Grace Yan <xgraceyan@gmail.com>
2026-01-14 23:36:32 -0800

    Finished experience section

--- src/components/home/Experience.tsx
+++ src/components/home/Experience.tsx
@@ -28,14 +28,14 @@ function ExperienceCard({ experience, key }: ExperienceCardProps) {
       className="flex flex-row hover:shadow-black/20 hover:shadow-xl hover:cursor-pointer transition-shadow duration-200"
       key={key}
     >
-      <div className="flex-shrink-0 flex justify-center items-center ps-6">
+      <div className="flex-shrink-0 ps-6">
         <img
           src={experience.imgUrl}
           alt={`${experience.company} logo`}
-          className="w-18 md:w-24 object-contain rounded-xl"
+          className="w-18 md:w-20 object-contain rounded-xl"
         />
       </div>
-      <div className="flex flex-col justify-center gap-2">
+      <div className="flex flex-col gap-2 justify-center">
         <CardHeader>
           <CardTitle>
             <p className="text-lg">{experience.company}</p>
@@ -91,12 +91,40 @@ export default function Experience() {
       ),
       imgUrl: "./logos/icssc.svg",
     },
+    {
+      id: 3,
+      company: "Ciphorama",
+      position: "Software Engineering Intern",
+      dates: "Jan 2023 - Aug 2023",
+      description: (
+        <p>
+          Interned for startup focusing on digital assets risk management. Built
+          front-end user interfaces using ReactJS with Ant Design and AntV G6
+          graph visualization engine.
+        </p>
+      ),
+      imgUrl: "./logos/ciphorama.jpeg",
+    },
+    {
+      id: 4,
+      company: "Cisco",
+      position: "Software Engineering Extern",
+      dates: "June 2022 - Aug 2022",
+      description: (
+        <p>
+          Interviewed senior engineers, shadowed life working at a large tech
+          company. Mentored by Cisco software engineer & researched into
+          detecting pneumonia in lung X-rays with AI
+        </p>
+      ),
+      imgUrl: "./logos/cisco.png",
+    },
   ];
 
   return (
     <div className="flex flex-col gap-4">
-      <h2 className="font-bold">Experience</h2>
-      <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
+      <h2 className="font-bold text-3xl">Experience</h2>
+      <div className="grid grid-cols-1 2xl:grid-cols-2 gap-4">
         {experiences.map((exp) => (
           <ExperienceCard experience={exp} key={exp.id} />
         ))}
